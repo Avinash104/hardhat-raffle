8f439c83f8a49df64c2c99be56d28f5ce344d581
alithegilfoyle <alimurtaza-assistant@outlook.com>
2022-08-10 09:44:59 +0500

    fix deploy issues

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,51 +1,50 @@
-require("@nomiclabs/hardhat-waffle")
-require("@nomiclabs/hardhat-etherscan")
-require("hardhat-deploy")
-require("solidity-coverage")
-require("hardhat-gas-reporter")
-require("hardhat-contract-sizer")
-require("dotenv").config()
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-deploy");
+require("solidity-coverage");
+require("hardhat-gas-reporter");
+require("hardhat-contract-sizer");
+require("dotenv").config();
 
-const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "RINKEBY"
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "KEY"
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "KEY"
+// const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "RINKEBY";
+// const PRIVATE_KEY = process.env.PRIVATE_KEY || "KEY";
+// const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "KEY";
 
 module.exports = {
-    solidity: "0.8.7",
-    defaultNetwork: "hardhat",
-    networks: {
-        hardhat: {
-            chainId: 31337,
-            blockConfirmations: 1,
-        },
-        rinkeby: {
-            chainId: 4,
-            blockConfirmations: 6,
-            url: RINKEBY_RPC_URL,
-            accounts: [PRIVATE_KEY],
-        },
+  solidity: "0.8.7",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      blockConfirmations: 1,
     },
-    etherscan: {
-        apiKey: {
-            rinkeby: ETHERSCAN_API_KEY,
-        },
+    rinkeby: {
+      url: process.env.RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 4,
+      blockConfirmations: 6,
+      gas: 6000000,
     },
-    gasReporter: {
-        enabled: false,
-        currency: "USD",
-        outputFile: "gas-report.txt",
-        noColors: true,
-        // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: false,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
     },
-    namedAccounts: {
-        deployer: {
-            default: 0,
-        },
-        player: {
-            default: 1,
-        },
+    player: {
+      default: 1,
     },
-    mocha: {
-        timeout: 300000,
-    },
-}
+  },
+  mocha: {
+    timeout: 300000,
+  },
+};
